698b3522d670e03a482497e9a65c41667271f951
PrachiGaikwad1305 <88362176+PrachiGaikwad1305@users.noreply.github.com>
2021-09-06 13:08:21 +0530

    Added Customer Registration

---
+++ 06/PetStore/Web/user-form.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="user-form.aspx.cs" Inherits="WebApplication1.user_form" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Customer Registration"></asp:Label>
+        <br />
+        <br />
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="tbName" runat="server" Width="226px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Email"></asp:Label>
+            <asp:TextBox ID="tbEmail" runat="server" Width="226px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Zipcode"></asp:Label>
+            <asp:TextBox ID="tbZipcode" runat="server" Width="209px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Username"></asp:Label>
+            <asp:TextBox ID="tbUsername" runat="server" Width="194px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label6" runat="server" Text="Password"></asp:Label>
+            <asp:TextBox ID="tbPassword" TextMode="Password" runat="server" Width="192px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="btnAdd_Click" />
+        </div>
+            
+    </form>
+
+    </body>
+</html>
---
+++ 06/PetStore/Web/user-form.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class user_form : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["conStr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                SqlCommand cmd = new SqlCommand("AddDetails", con);
+                
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", tbName.Text);
+                cmd.Parameters.AddWithValue("@Zipcode", tbZipcode.Text);
+                cmd.Parameters.AddWithValue("@Email", tbEmail.Text);
+                cmd.Parameters.AddWithValue("@UserName", tbUsername.Text);
+                cmd.Parameters.AddWithValue("@Password", tbPassword.Text);
+
+                SqlParameter outputpara = new SqlParameter();
+                outputpara.ParameterName = "@Id";
+                outputpara.SqlDbType = SqlDbType.Int;
+                outputpara.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(outputpara);
+
+                SqlParameter outputpara1 = new SqlParameter();
+                outputpara1.ParameterName = "@loginId";
+                outputpara1.SqlDbType = SqlDbType.Int;
+                outputpara1.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(outputpara1);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                tbName.Text = "";
+                tbEmail.Text = "";
+                tbZipcode.Text = "";
+                tbUsername.Text = "";
+                tbPassword.Text = "";
+                tbName.Focus();
+
+            }
+
+        }
+
+        
+    }
+}
